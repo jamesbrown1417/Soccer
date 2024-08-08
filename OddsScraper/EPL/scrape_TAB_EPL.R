594afb9e080b1eb9924dc94704584cf0a52ffd4e
jamesbrown1417 <james.brown1417@gmail.com>
2024-08-08 13:04:05 +0930

    latest updates

---
+++ OddsScraper/EPL/scrape_TAB_EPL.R
@@ -0,0 +1,169 @@
+# Libraries
+library(tidyverse)
+library(rvest)
+library(httr2)
+library(jsonlite)
+
+# URL to get responses
+tab_url = "https://api.beta.tab.com.au/v1/tab-info-service/sports/Soccer/competitions/English%20Premier%20League?jurisdiction=SA"
+
+# Function to fetch and parse JSON with exponential backoff
+fetch_data_with_backoff <-
+    function(url,
+             delay = 1,
+             max_retries = 5,
+             backoff_multiplier = 2) {
+        tryCatch({
+            # Attempt to fetch and parse the JSON
+            tab_response <-
+                read_html_live(url) |>
+                html_nodes("pre") %>%
+                html_text() %>%
+                fromJSON(simplifyVector = FALSE)
+            
+            # Return the parsed response
+            return(tab_response)
+        }, error = function(e) {
+            if (max_retries > 0) {
+                # Log the retry attempt
+                message(sprintf(
+                    "Error encountered. Retrying in %s seconds...",
+                    delay
+                ))
+                
+                # Wait for the specified delay
+                Sys.sleep(delay)
+                
+                # Recursively call the function with updated parameters
+                return(
+                    fetch_data_with_backoff(
+                        url,
+                        delay * backoff_multiplier,
+                        max_retries - 1,
+                        backoff_multiplier
+                    )
+                )
+            } else {
+                # Max retries reached, throw an error
+                stop("Failed to fetch data after multiple retries.")
+            }
+        })
+    }
+
+tab_response <- fetch_data_with_backoff(tab_url)
+
+# Function to extract market info from response---------------------------------
+get_market_info <- function(markets) {
+    # Market info
+    markets_name = markets$betOption
+    market_propositions = markets$propositions
+    
+    # Output Tibble
+    tibble(market = markets_name, propositions = market_propositions)
+}
+
+# Function to extract match info from response----------------------------------
+get_match_info <- function(matches) {
+    # Match info
+    match_name = matches$name
+    match_start_time = matches$startTime
+    
+    # Market info
+    market_info = map(matches$markets, get_market_info) |> bind_rows()
+    
+    # Output Tibble
+    tibble(
+        match = match_name,
+        start_time = match_start_time,
+        market_name = market_info$market,
+        propositions = market_info$propositions
+    )
+}
+
+# Map functions to data
+all_tab_markets <-
+    map(tab_response$matches, get_match_info) |> bind_rows()
+
+# Expand list col into multiple cols
+all_tab_markets <-
+    all_tab_markets |>
+    unnest_wider(col = propositions, names_sep = "_") |>
+    select(any_of(c("match", "start_time", "market_name")),
+           prop_name = propositions_name,
+           prop_id = propositions_id,
+           price = propositions_returnWin)
+
+#===============================================================================
+# Head to head markets
+#===============================================================================
+
+# Home teams
+home_teams <-
+    all_tab_markets |>
+    separate(
+        match,
+        into = c("home_team", "away_team"),
+        sep = " v ",
+        remove = FALSE
+    ) |>
+    filter(market_name == "Result") |>
+    group_by(match) |>
+    filter(row_number() == 1) |>
+    rename(home_win = price) |>
+    select(-prop_name, -prop_id)
+
+# Draw
+draw_h2h <-
+    all_tab_markets |>
+    separate(
+        match,
+        into = c("home_team", "away_team"),
+        sep = " v ",
+        remove = FALSE
+    ) |>
+    filter(market_name == "Result") |>
+    group_by(match) |>
+    filter(row_number() == 2) |>
+    rename(draw = price) |>
+    select(-prop_name, -prop_id)
+
+# Away teams
+away_teams <-
+    all_tab_markets |>
+    separate(
+        match,
+        into = c("home_team", "away_team"),
+        sep = " v ",
+        remove = FALSE
+    ) |>
+    filter(market_name == "Result") |>
+    group_by(match) |>
+    filter(row_number() == 3) |>
+    rename(away_win = price) |>
+    select(-prop_name, -prop_id)
+
+# Combine
+tab_head_to_head_markets <-
+    home_teams |>
+    left_join(draw_h2h) |>
+    left_join(away_teams) |>
+    select(match,
+           start_time,
+           market_name,
+           home_team,
+           home_win,
+           draw,
+           away_team,
+           away_win) |>
+    mutate(margin = round((1 / home_win + 1 / away_win + 1 / draw), digits = 3)) |>
+    mutate(agency = "TAB")
+
+# Fix team names
+tab_head_to_head_markets <-
+    tab_head_to_head_markets |>
+    mutate(home_team = fix_team_names(home_team)) |>
+    mutate(away_team = fix_team_names(away_team)) |>
+    mutate(match = paste(home_team, "v", away_team))
+
+# Write to csv
+write_csv(tab_head_to_head_markets, "Data/scraped_odds/tab_h2h.csv")
---
+++ Scripts/get_epl_data_fbref.R
@@ -0,0 +1,182 @@
+#===============================================================================
+# Set up
+#===============================================================================
+
+# Libraries
+library(worldfootballR)
+library(tidyverse)
+library(furrr)
+
+# Set up parallel processing
+plan(multisession)
+
+#===============================================================================
+# Team Stats
+#===============================================================================
+
+# Function to get team stats for a given year-----------------------------------
+get_team_stats <- function(season) {
+
+# Get match results
+epl_results <-
+    load_match_results(
+        country = "ENG",
+        gender = "M",
+        tier = "1st",
+        season_end_year = season
+    ) |>
+    transmute(
+        Competition = "EPL",
+        Season = paste(Season_End_Year - 1, Season_End_Year, sep = "/"),
+        Week = Wk,
+        Match = paste(Home, "v", Away),
+        MatchURL,
+        Date,
+        Time,
+        Venue,
+        Attendance,
+        Referee,
+        Home,
+        HomeGoals,
+        Home_xG,
+        Away,
+        AwayGoals,
+        Away_xG
+    )
+
+# Get match summary stats
+epl_match_summary_stats_all <- load_fb_advanced_match_stats(
+    country = "ENG",
+    gender = "M",
+    tier = "1st",
+    season_end_year = season,
+    stat_type = "summary",
+    team_or_player = "team"
+)
+
+epl_match_summary_stats <-
+    epl_match_summary_stats_all |>
+    transmute(
+        MatchURL,
+        Home_Formation,
+        Home_Scorers = Home_Goals,
+        Home_Yellow_Cards,
+        Home_Red_Cards,
+        Away_Formation,
+        Away_Scorers = Away_Goals,
+        Away_Yellow_Cards,
+        Away_Red_Cards,
+        Team,
+        Home_Away,
+        Assists = Ast,
+        Shots = Sh,
+        Shots_On_Target = SoT,
+        Tackles = Tkl,
+        Interceptions = Int,
+        Passes = Cmp_Passes,
+        Passes_Attempted = Att_Passes,
+        Pass_Completion_Rate = Cmp_percent_Passes)
+
+# Join with match results
+epl_match_summary_stats_final <-
+    epl_results |>
+    left_join(epl_match_summary_stats, by = "MatchURL") |>
+    relocate(Home_Formation,
+             Home_Scorers,
+             Home_Yellow_Cards,
+             Home_Red_Cards,
+             .after = "Home_xG") 
+
+return(epl_match_summary_stats_final)
+}
+
+# Get team stats for the last 5 seasons
+epl_team_stats <-
+    future_map(2020:2024, get_team_stats, .progress = TRUE) |>
+    bind_rows()
+
+#===============================================================================
+# Player Stats
+#===============================================================================
+
+# Function to get player stats for a given year---------------------------------
+get_player_stats <- function(season) {
+    # Get match results
+    epl_results <-
+        load_match_results(
+            country = "ENG",
+            gender = "M",
+            tier = "1st",
+            season_end_year = season
+        ) |>
+        transmute(
+            Competition = "EPL",
+            Season = paste(Season_End_Year - 1, Season_End_Year, sep = "/"),
+            Week = Wk,
+            Match = paste(Home, "v", Away),
+            MatchURL,
+            Date,
+            Time,
+            Venue,
+            Attendance,
+            Referee,
+            Home,
+            HomeGoals,
+            Home_xG,
+            Away,
+            AwayGoals,
+            Away_xG
+        )
+    
+    # Get player summary stats
+    epl_player_summary_stats <-
+        load_fb_advanced_match_stats(
+            country = "ENG",
+            gender = "M",
+            tier = "1st",
+            season_end_year = season,
+            stat_type = "summary",
+            team_or_player = "player"
+        ) |>
+        transmute(
+            MatchURL,
+            Player,
+            Player_Team = Team,
+            Home_Away,
+            Player_Number = Player_Num,
+            Position = Pos,
+            Minutes = Min,
+            Yellow_Cards = CrdY,
+            Red_Cards = CrdR,
+            Goals = Gls,
+            Assists = Ast,
+            Shots = Sh,
+            Shots_On_Target = SoT,
+            Tackles = Tkl,
+            Interceptions = Int,
+            Passes = Cmp_Passes,
+            Passes_Attempted = Att_Passes,
+            Pass_Completion_Rate = Cmp_percent_Passes
+        )
+    
+    # Combine
+    epl_player_stats <-
+        epl_results |>
+        left_join(epl_player_summary_stats, by = "MatchURL")
+    
+    # Return
+    return(epl_player_stats)
+}
+
+# Get player stats for the last 5 seasons
+epl_player_stats <-
+    future_map(2020:2024, get_player_stats, .progress = TRUE) |>
+    bind_rows()
+
+#===============================================================================
+# Write out data to RDS
+#===============================================================================
+
+# Write out team stats
+write_rds(epl_team_stats, "data/epl_team_stats.rds")
+write_rds(epl_player_stats, "data/epl_player_stats.rds")
